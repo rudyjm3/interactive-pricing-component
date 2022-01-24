560a22144b9880a38a21f7f41b977ae57ac7f07b
rudyjm3 <rudyjm3@yahoo.com>
2022-01-24 12:04:50 -0500

    styling and js function update for toggle.

--- js/app.js
+++ js/app.js
@@ -9,3 +9,19 @@ slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(
 slider.oninput = function() {
   this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB 100%)`
 };
+
+// TOGGLE
+let toggle = document.getElementById('chk');
+
+toggle.addEventListener('change', e => {
+
+   if(e.target.checked){
+      document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+      document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Grayish_Blue)";
+      document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: rgba(254, 236, 231, 1.0); transition: all 0.5s ease-in-out";
+   } else {
+      document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+      document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Grayish_Blue)";
+      document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: transparent";
+   }
+})
