fd822bda26e8ecc760bccaead1cb380668fa5f3b
rudyjm3 <rudyjm3@yahoo.com>
2022-01-24 20:25:46 -0500

    updated favicons, and style updates

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ const min = slider.min
 const max = slider.max
 const value = slider.value
 
-slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(value-min)/(max-min)*100}%, #E9EEFB ${(value-min)/(max-min)*100}%, #E9EEFB 100%)`
+slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 96%, 42%) ${(value-min)/(max-min)*100}%, #E9EEFB ${(value-min)/(max-min)*100}%, #E9EEFB 100%)`
 
 slider.oninput = function() {
-  this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB 100%)`
+  this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 96%, 42%) ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB ${(this.value-this.min)/(this.max-this.min)*100}%, #E9EEFB 100%)`
 };
 
 
