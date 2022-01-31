ee304d1623f3bdae3c5d45824ff745b59b20e51a
rudyjm3 <rudyjm3@yahoo.com>
2022-01-30 19:05:45 -0500

    Feat-- Added percent of switch function. Refactor toggle function into the switch function.

--- js/app.js
+++ js/app.js
@@ -11,25 +11,7 @@ slider.oninput = function() {
 };
 
 
-// TOGGLE
-let toggle = document.getElementById('chk');
-
-toggle.addEventListener('change', e => {
-
-   if(e.target.checked){
-      document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
-      document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Grayish_Blue)";
-      document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: rgba(254, 236, 231, 1.0); transition: all 0.5s ease-in-out";
-   } else {
-      document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
-      document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Grayish_Blue)";
-      document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: transparent";
-   }
-
-})
-
-
-// PRICE CALC
+// SLIDER  /////////////////////////////////////////////////////////
 let viewers = document.getElementsByClassName('views_number')[0];
 let cost = document.getElementsByClassName('m_amount')[0];
 
@@ -39,35 +21,93 @@ function moneySliderUpdate(ele) {
    console.log(value);
 
    switch(value) {
+      case 0:
+         view = "0";
+         cost = "0.00"; 
       case 20:
          view = "10K";
-         cost = "$8.00";
+         cost = "8.00";
+         discount = (25 / 100) * cost;
+         discount_amount = cost - discount; 
          break;
       case 40:
          view = "50K";
-         cost = "$12.00";
+         cost = "12.00"; 
+         discount = (25 / 100) * cost;
+         discount_amount = cost - discount;          
          break;
       case 60:
          view = "100K";
-         cost = "$16.00";
+         cost = "16.00";
+         discount = (25 / 100) * cost;
+         discount_amount = cost - discount; 
          break;
       case 80:
          view = "500K";
-         cost = "$24.00";
+         cost = "24.00";
+         discount = (25 / 100) * cost;
+         discount_amount = cost - discount; 
          break;
       case 100:
          view = "1M";
-         cost = "$36.00";
+         cost = "36.00";
+         discount = (25 / 100) * cost;
+         discount_amount = cost - discount;
          break;  
-      case 0:
-         view = "0";
-         cost = "$0.00"; 
    }
+
    document.getElementsByClassName('views_number')[0].innerText = view;
    console.log(view);
    document.getElementsByClassName('m_amount')[0].innerText = cost;
    console.log(cost);
-}
+
+
+   let toggle = document.getElementById('chk');
+   toggle.addEventListener('change', e => {
+
+      if(e.target.checked) {
+         document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+         document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Grayish_Blue)";
+         document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: rgba(254, 236, 231, 1.0); transition: all 0.5s ease-in-out";
+
+         document.getElementsByClassName('m_amount')[0].innerText = discount_amount;
+
+         console.log("cost minus discount, inside switch statement " + discount_amount);
+         console.log("discount calc inside switch statement " + discount);
+      } else {
+         document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+         document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Grayish_Blue)";
+         document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: transparent";
+
+         document.getElementsByClassName('m_amount')[0].innerText = cost;
+      }
+   })
+};
+
+////////////////////////////////////////////////////////
+// TOGGLE
+// let toggle = document.getElementById('chk');
+
+// toggle.addEventListener('change', e => {
+
+//    let currentCost = document.getElementsByClassName('m_amount')[0].innerText;
+//    let discount = 25;
+//    let distotal;
+//    let percent;
+// console.log(currentCost + " unchanged amount");
+
+//    if(e.target.checked){
+//       document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+//       document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Grayish_Blue)";
+//       document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: rgba(254, 236, 231, 1.0); transition: all 0.5s ease-in-out";
+      
+//    } else {
+//       document.getElementsByClassName('mth')[0].style.cssText = "color:var(--Strong_Cyan); transition: color 0.5s ease-in-out";
+//       document.getElementsByClassName('yr')[0].style.cssText = "color:var(--Grayish_Blue)";
+//       document.getElementsByClassName('discount__amount')[0].style.cssText ="background-color: transparent";
+//    }
+// })
+
 
 
 // Here are the different page view ranges and the corresponding monthly price totals:
@@ -79,3 +119,5 @@ function moneySliderUpdate(ele) {
 // - 1M pageviews / $36 per month (slider step = 100)
 
 // If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+
+// DISCOUNT
